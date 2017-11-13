c5fd2ab5576216e3fbe5d9f4a2e743d2107b11be
Sauroctone <neo.laq.33@gmail.com>
2017-11-13 19:21:41 +0100

    Début proto gameplay joueurs
    - L'objet mousecontroller suit la souris
    - les zones de CC et heal s'activent
    EN COURS : la mécanique du CC

---
+++ Assets/Scripts/Mouse/MouseController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController: MonoBehaviour {
+
+	public GameObject healZone;
+	public GameObject ccZone;
+
+	bool timerIsTicking;
+	public float ccTimer;
+
+	Vector3 mouseScreenPos;
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown (1)) 
+		{
+			healZone.SetActive (true);
+			ccZone.SetActive (false);
+		}
+
+		if (Input.GetMouseButtonUp (1)) 
+		{
+			healZone.SetActive (false);
+		}
+			
+
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			if (timerIsTicking) 
+			{
+				ccZone.SetActive (false);
+				Pull ();
+			} 
+
+			else 
+			{
+				healZone.SetActive (false);
+				ccZone.SetActive (true);
+
+				StartCoroutine ("CrowdControlTimer");
+			}
+
+		}
+	}
+
+	void Pull ()
+	{
+
+	}
+
+	IEnumerator CrowdControlTimer ()
+	{
+		timerIsTicking = true;
+		yield return new WaitForSeconds (ccTimer);
+		timerIsTicking = false;
+	}
+}
